7c09cb0a54654591f695cf3160b26ca8536ae1dd
koutarou-shinnzaki <kokutarou0322@yahoo.co.jp>
2022-09-27 12:21:20 +0900

    Laravelアプリケーション初期設定” ;

--- database/migrations/2022_09_27_110539_create_posts_table.php
+++ database/migrations/2022_09_27_110539_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+          Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('body', 200);
+            $table->timestamps();
+            $table->softDeletes();
         });
+        
     }
 
     /**
@@ -27,6 +30,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('posts');
     }
 }
+
+
+
+
+      
+
+    
